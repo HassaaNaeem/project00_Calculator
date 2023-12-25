f3136dbd451098e4d8f32dd6dd32c5337851c65f
HassaaNaeem <Hassaan282006@gmail.com>
2023-12-25 21:49:28 +0500

    Project Calculator

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log("Hello");
+import inquirer from 'inquirer';
+let runAgain = 1;
+for (let i = 0; i < runAgain; i++) {
+    let user = await inquirer.prompt([
+        {
+            type: "number",
+            name: "FirstNumber",
+            message: "Enter First Number: "
+        },
+        {
+            type: "list",
+            name: "Operator",
+            message: "Select Operator (+, -, *, /, **) ",
+            choices: ['Addition (+)', 'Subtraction (-)', 'Multiplication (*)', 'Division (/)', 'Exponent (**)']
+        },
+        {
+            type: "number",
+            name: "SecondNumber",
+            message: "Enter Second Number: "
+        },
+    ]);
+    if (user.Operator == 'Addition (+)') {
+        console.log(`The result of  ${user.FirstNumber} + ${user.SecondNumber} = ${user.FirstNumber + user.SecondNumber}`);
+    }
+    else if (user.Operator == 'Subtraction (-)') {
+        console.log(`The result of  ${user.FirstNumber} - ${user.SecondNumber} = ${user.FirstNumber - user.SecondNumber}`);
+    }
+    else if (user.Operator == 'Multiplication (*)') {
+        console.log(`The result of  ${user.FirstNumber} * ${user.SecondNumber} = ${user.FirstNumber * user.SecondNumber}`);
+    }
+    else if (user.Operator == 'Division (/)') {
+        console.log(`The result of  ${user.FirstNumber} / ${user.SecondNumber} = ${user.FirstNumber / user.SecondNumber}`);
+    }
+    else if (user.Operator == 'Exponent (**)') {
+        console.log(`The result of  ${user.FirstNumber} ** ${user.SecondNumber} = ${user.FirstNumber ** user.SecondNumber}`);
+    }
+    else {
+        console.log("You have entered invalid number");
+    }
+    let question = await inquirer.prompt({
+        type: "list",
+        name: "again",
+        message: "Do you want to calculate again?",
+        choices: ['Yes', 'No']
+    });
+    if (question.again == 'Yes') {
+        runAgain += 1;
+    }
+    else {
+        break;
+    }
+}
